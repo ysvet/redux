490aef555b3c688019460421999a0f872e906821
ysvet <iurie.shvets@gmail.com>
2019-05-16 21:31:37 +0300

    immutably update arrays

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -30,6 +30,16 @@ const reducer = (state = initialState, action) => {
             ...state,
            results: state.results.concat({id: new Date(), value: state.counter})
         }    
+    case 'DELETE_RESULT':
+        //one of the way to update immutably array:
+        // const id = 2;
+        // const newArray = [...state.results];
+        // newArray.splice(id, 1);
+        const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+        return {
+            ...state,
+           results: updatedArray
+        }    
     }
     return state;
 };
